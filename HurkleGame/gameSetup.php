934340a65b979d5856fd301429a66a8cffc82f63
rpeacock93 <rpeacock@csumb.edu>
2018-04-10 20:55:35 -0700

    current hurkle game

---
+++ HurkleGame/gameSetup.php
@@ -0,0 +1,58 @@
+<?php 
+
+
+
+function createLihrt() {
+	$arrlength = 10;
+
+	$lihrt = array 
+	(
+		array(),array(),array(),array(),array(),
+		array(),array(),array(),array(),array()
+	);
+
+	echo "<span id='board'>";
+
+	for ($row = 0; $row < $arrlength; $row++) {
+	        for ($col = 0; $col < $arrlength; $col++) {
+
+	        	if ($row == ($_POST["guessX"] -1) && $col == ($_POST["guessY"] - 1)) {
+
+	        		echo $lihrt[$row][$col] = "<span id='hurkle'>&block;&nbsp;&nbsp;</span>";
+
+	        	}
+	        	else {
+
+	        		echo $lihrt[$row][$col] = '&block;&nbsp;&nbsp;';
+	        	}
+	        }
+	        echo "<br>";
+	    }
+	    echo "</span>";
+
+	    echo "<br>";
+
+	}
+
+function hidHurkle() {
+
+	$xCoord = rand(1, 10);
+	$yCoord = rand(1, 10);
+
+}
+
+function getGuess() {
+
+	$guessX = $_POST["guessX"];
+	$guessY = $_POST["guessY"];
+
+
+	echo '<span class="output"> Your guess: ', '( ', $_POST["guessX"], ' , ', $_POST["guessY"], ' ) </span><br>' ;
+	echo '<span class="output"> Hint: </span>' ;
+
+}	
+
+
+
+
+ ?>
---
+++ HurkleGame/index.php
@@ -0,0 +1,52 @@
+<?php include('gameSetup.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My title</title>
+	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+	<body>
+
+
+
+		<div id="container" >
+
+			
+			<h1 class="strokeme">Find Hurkle</h1>
+
+			<span class="output">Wins: Losses: </span>
+			<br>
+			<br>
+
+			<?php 
+
+
+			// Having trouble with a defualt POST value, but needed to get something going
+			// So i could start styling the page
+			// $_POST['guessX'] = 5;
+			// $_POST['guessY'] = 5;
+			createLihrt();
+			getGuess();	
+
+			 ?>
+
+			 <br>
+			 <br>
+
+
+			 <form action="index.php" method="post">
+	            <span class="guess">X: </span><input class="guess" type="text" name="guessX">
+	            <span class="guess">Y: </span><input class="guess" type="text" name="guessY"><br>
+	            <br>
+	            <input id="button" type="submit">
+	        </form>
+
+			 
+
+		</div>
+
+	</body>
+</html>
